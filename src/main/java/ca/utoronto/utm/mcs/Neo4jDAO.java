4af4806f5ed54880eb8b8c2c17ed18fca3a36540
Arya Sharma <73757085+Arya2504s@users.noreply.github.com>
2022-06-15 11:34:06 -0400

    Completed hasRelationship

--- src/main/java/ca/utoronto/utm/mcs/Neo4jDAO.java
+++ src/main/java/ca/utoronto/utm/mcs/Neo4jDAO.java
@@ -7,6 +7,8 @@ import org.neo4j.driver.*;
 import org.neo4j.driver.Record;
 import org.neo4j.driver.exceptions.Neo4jException;
 
+import java.util.ArrayList;
+
 public class Neo4jDAO implements AutoCloseable{
     // TODO Complete This Class
     private final Session session;
@@ -28,6 +30,19 @@ public class Neo4jDAO implements AutoCloseable{
         System.out.println(result.consume());
     }
 
+    public ArrayList<String> getRelation(String query) throws Neo4jException {
+        Result result = this.session.run(query);
+        ArrayList<String> res = new ArrayList<String>();
+        String actorId = "";
+        String movieId = "";
+        for(Record rec: result.list()){
+            actorId = rec.get("a.actorId").toString();
+            movieId = rec.get("b.movieId").toString();
+            res.add(actorId);
+            res.add(movieId);
+        }
+        return res;
+    }
     public void initialSetup(){
         String query1 =
             "CREATE CONSTRAINT unique_actorId FOR (actor: Actor) REQUIRE actor.actorId IS UNIQUE";
